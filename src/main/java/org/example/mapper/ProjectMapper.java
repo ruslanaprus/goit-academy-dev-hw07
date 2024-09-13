6429e93959ffdbd144dbab93594e62ed7d674241
ruslana <ruslana.prus@ukr.net>
2024-09-13 13:58:31 +0200

    mapper for sqlite

--- src/main/java/org/example/mapper/ProjectMapper.java
+++ src/main/java/org/example/mapper/ProjectMapper.java
@@ -5,14 +5,18 @@ import org.example.model.Project;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.time.format.DateTimeFormatter;
 
 public class ProjectMapper implements EntityMapper<Project> {
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Override
     public void mapToStatement(PreparedStatement statement, Project project) throws SQLException {
         statement.setString(1, project.getName());
         statement.setInt(2, project.getClient_id());
-        statement.setDate(3, Date.valueOf(project.getStart_date()));
-        statement.setDate(4, Date.valueOf(project.getFinish_date()));
+        statement.setObject(3, project.getStart_date().format(DATE_FORMATTER));
+        statement.setObject(4, project.getFinish_date().format(DATE_FORMATTER));
+//        statement.setDate(3, Date.valueOf(project.getStart_date()));
+//        statement.setDate(4, Date.valueOf(project.getFinish_date()));
     }
 }
--- src/main/java/org/example/mapper/WorkerMapper.java
+++ src/main/java/org/example/mapper/WorkerMapper.java
@@ -5,12 +5,16 @@ import org.example.model.Worker;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Date;
+import java.time.format.DateTimeFormatter;
 
 public class WorkerMapper implements EntityMapper<Worker> {
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
     @Override
     public void mapToStatement(PreparedStatement statement, Worker worker) throws SQLException {
         statement.setString(1, worker.getName());
-        statement.setDate(2, Date.valueOf(worker.getDateOfBirth()));
+        statement.setObject(2, worker.getDateOfBirth().format(DATE_FORMATTER));
+//        statement.setDate(2, Date.valueOf(worker.getDateOfBirth()));
         statement.setString(3, worker.getEmail());
         statement.setString(4, worker.getLevel().name().toLowerCase());
         statement.setInt(5, worker.getSalary());
